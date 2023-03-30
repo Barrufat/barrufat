8950c685d080cd9bb20533a2f4ae6b2fb3639a31
Alex Barbero <barrufat@hotmail.com>
2023-03-30 17:26:44 +0200

    retoques CSS

--- src/componentes/aPresentacion.js
+++ src/componentes/aPresentacion.js
@@ -6,7 +6,48 @@ const Presentacion = () => {
             <div className='cartaTitulo'>
                 <img className='presentacionImg' src='./emojiBarru.png' alt='jetoTITULO'></img>
                 <div className='contTextoPresentacion'>
-                    <h1 className='presentacionTitulo'>Diseñador & Desarrollador Web Full Stack</h1>
+                    <div className='contTitulo'>
+                        <div className='presentacionTitulo'>
+                            <div className='letraNaranja'>D</div>
+                            <div className='letraVioleta'>i</div>
+                            <div className='letraVerde'>s</div>
+                            <div className='letraNaranja'>e</div>
+                            <div className='letraVioleta'>ñ</div>
+                            <div className='letraVerde'>a</div>
+                            <div className='letraNaranja'>d</div>
+                            <div className='letraVioleta'>o</div>
+                            <div className='letraVerde'>r</div>
+                            <div className='letraNaranja'>&</div>
+                        </div>
+                        <div className='presentacionTitulo'>
+                            <div className='letraVioleta'>D</div>
+                            <div className='letraVerde'>e</div>
+                            <div className='letraNaranja'>s</div>
+                            <div className='letraVioleta'>a</div>
+                            <div className='letraVerde'>r</div>
+                            <div className='letraNaranja'>r</div>
+                            <div className='letraVioleta'>o</div>
+                            <div className='letraVerde'>l</div>
+                            <div className='letraNaranja'>l</div>
+                            <div className='letraVioleta'>a</div>
+                            <div className='letraVerde'>d</div>
+                            <div className='letraNaranja'>o</div>
+                            <div className='letraVioleta'>r</div>
+                        </div>
+                        <div className='presentacionTitulo'>
+                            <div className='letraVerde'>F</div>
+                            <div className='letraNaranja'>u</div>
+                            <div className='letraVioleta'>l</div>
+                            <div className='letraVerde'>l</div>
+                        </div>
+                        <div className='presentacionTitulo'>
+                            <div className='letraNaranja'>S</div>
+                            <div className='letraVioleta'>t</div>
+                            <div className='letraVerde'>a</div>
+                            <div className='letraNaranja'>c</div>
+                            <div className='letraVioleta'>k</div>
+                        </div>
+                    </div>
                     <h2 className='presentacionTexto'>Me gusta ayudar a empresas a crear su espacio en la red y disfruto desarrollando aplicaciones e interactivos. </h2>
                 </div>
             </div>
